db0f12b593b6e783032639708d6e2ed7b18b55bf
Sagar Dagdu <shags032@gmail.com>
2023-08-29 20:56:33 +0530

    [FLEET-600] Add initial skeleton app

---
+++ FairmaticInsuranceSample/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FairmaticInsuranceSample
+//
+//  Created by Sagar Dagdu on 29/08/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    /// The main window
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
